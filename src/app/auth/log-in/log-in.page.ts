18024a7a1c9a57dde00b41985e7c1939aa9bfc27
Marija Rajkovic <marijaaa.majaaa@gmail.com>
2025-09-30 14:42:15 +0200

    Spinner

--- src/app/auth/log-in/log-in.page.ts
+++ src/app/auth/log-in/log-in.page.ts
@@ -11,6 +11,7 @@ import { LoadingController } from '@ionic/angular';
   standalone: false
 })
 export class LogInPage implements OnInit {
+  isLoading = false;
 
   constructor(private authservice: AuthService, private router: Router, private loadingController: LoadingController) { }
 
@@ -18,20 +19,23 @@ export class LogInPage implements OnInit {
   }
 
   onLogIn(logInForm: NgForm){
+    this.isLoading = true;
+    /*
     this.loadingController
       .create({message: "Log in ..."})
       .then((loadingEl: HTMLIonLoadingElement) => {
         loadingEl.present();
-
+    */
       if(logInForm.valid){
         this.authservice.logIn(logInForm.value).subscribe(resData => {
           console.log('Prijava uspesna');
           console.log(resData);
-          loadingEl.dismiss();
+          this.isLoading = false;
+          /*loadingEl.dismiss();*/
           this.router.navigateByUrl('/destination/tabs/explore');
         });
       }
-    });
+    //});
   }
 
 }
